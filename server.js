f14f44f637a4645bac17afbee910e64b19428843
ehidalgomartos-png <ehidalgomartos@gmail.com>
2026-09-15 12:19:26 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,27 +6,29 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Sirve la página web principal
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// Gestión de conexiones y turnos
 io.on('connection', (socket) => {
   console.log('¡Nuevo jugador conectado:', socket.id);
 
-  // Reenvía la acción elegida (Verdad o Reto) al oponente
-  socket.on('accion_juego', (tipoAccion) => {
-    socket.broadcast.emit('actualizar_mesa', tipoAccion);
+  // 1. Unirse a una sala privada
+  socket.on('unirse_sala', (salaID) => {
+    socket.join(salaID);
+    console.log(`Jugador se unió a la sala secreta: ${salaID}`);
+    // Avisa al creador de que su amigo ha entrado
+    socket.to(salaID).emit('oponente_unido');
   });
 
-  // Reenvía la foto-prueba al oponente
-  socket.on('enviar_foto', (fotoBase64) => {
-    socket.broadcast.emit('recibir_foto', fotoBase64);
+  // 2. Enviar carta SOLO al rival de tu sala
+  socket.on('accion_juego', (datos) => {
+    socket.to(datos.sala).emit('actualizar_mesa', datos);
   });
 
-  socket.on('disconnect', () => {
-    console.log('Un jugador se ha desconectado');
+  // 3. Enviar foto SOLO al rival de tu sala
+  socket.on('enviar_foto', (datos) => {
+    socket.to(datos.sala).emit('recibir_foto', datos.fotoBase64);
   });
 });
 
